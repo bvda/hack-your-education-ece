1664708e9bc215518227d36e37e827a1850a1b16
Henrik Kirk <henrikkirk@gmaill.com>
2021-10-05 11:05:32 +0200

    Added simple project to calculate half a square

---
+++ code/problems/square/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System;
+
+namespace square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Int32.Parse(args[0]);
+
+            for (int row = 1; row <= size; row++) {
+                for (int j = ; j <= (size+1) - row; j++) {
+                    System.Console.Write("#");
+                }
+                System.Console.WriteLine("");
+            }
+        }
+    }
+}
