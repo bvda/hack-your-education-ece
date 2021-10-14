20a6deba292de21ca5d7b7353b44077c2e60bb5f
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2021-10-14 23:03:10 +0200

    Huge (read: shitty) commit

---
+++ code/debugging/fizzbuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> range = Enumerable.Range(0, Int32.Parse(args[0]));
+            foreach(var i in range) {
+                if(i % 3 == 0 || i % 5 == 0) {
+                    Console.WriteLine("Fizz");
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
