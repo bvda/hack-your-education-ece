3abfcebee905b37643f7d6d6858956e80696f865
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2021-11-10 17:10:04 +0100

    Updated .gitignore and clean up

--- code/scripting/PipeOutput.ps1
+++ code/scripting/PipeOutput.ps1
@@ -1,14 +1,17 @@
-# $expression = &"FizzBuzz_osx-x64 0 100 3 4";
+Set-Location "/Users/au299473/Workspace/hack-your-cs-education/code/scripting/"
+New-Item "./out" -ItemType "directory" -Force
 
-$fizzbuzz = (3,5), (5,7), (7,9), (11,13)
+# Declare 'variables'
+$fizzbuzz = (3,5), (5,7), (7,9), (11,13), (13, 15)
 $start = 0
 $end = 1000
 
+# Generate output
 foreach($t in $fizzbuzz) {
   $fizz = $t.Get(0)
   $buzz = $t.Get(1)
   Start-Process `
-    -FilePath "/Users/au299473/Workspace/hack-your-cs-education/code/scripting/FizzBuzz_osx-x64" `
+    -FilePath "./FizzBuzz_osx-x64" `
     -ArgumentList $start, $end, $fizz, $buzz `
-    -RedirectStandardOutput "$fizz-$buzz.out"
+    -RedirectStandardOutput "./out/$fizz-$buzz.out"
 }
