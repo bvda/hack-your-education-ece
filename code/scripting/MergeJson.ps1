e42160c698f577a45d406ef27a04811e8e3b3aaa
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2021-11-10 19:30:21 +0100

    Update merge JSON

--- code/scripting/MergeJson.ps1
+++ code/scripting/MergeJson.ps1
@@ -1,14 +1,21 @@
+# Set current working directory
 Set-Location -Path /Users/bvd/Workspace/hack-your-cs-education/code/scripting/data
 
-# Write file content to out_file
-# $out_file = New-Item -Path '../out_file.txt' -Force
-# foreach($file in Get-ChildItem) {
-#   Get-Content $file | Out-File -Path $out_file -Append
-# }
+# Create ArrayList
+$result_base = New-Object System.Collections.ArrayList
 
+# Loop through files in current working directory
 foreach($file in Get-ChildItem) {
+  
+  # Convert file content to JSON
   $json = Get-Content $file | ConvertFrom-Json
+
+  # Loop through each entry in JSON array
   foreach($person in $json) {
-    Write-Host $person
+    $result_base.Add($person)
   }
+
 }
+
+# Convert Array to JSON and write to file
+$result_base | ConvertTo-Json | Out-File 'merged.json'
