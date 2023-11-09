afb7c1951db0dfd51e4448d51ec794ef5e6cf154
Brian Danielsen <ctrl@hey.com>
2023-11-09 13:53:03 +0100

    Add more stuff

--- code/debugging/04 Hardcore/04.02 Encoding.py
+++ code/debugging/04 Hardcore/04.02 Encoding.py
@@ -1,5 +1,6 @@
 # import csv
 import csv
+from cash_register.item import purchase_items
 
 # PATH = ''
 PATH = 'code/debugging/04 Hardcore/cities.csv'
@@ -8,3 +9,7 @@ PATH = 'code/debugging/04 Hardcore/cities.csv'
 with open(PATH, 'r', encoding='utf-8') as f:
   data = csv.DictReader(f)
   [print(row) for row in data]
+
+price = purchase_items(input("Enter # of items: "), input("Enter price per item: "), input("Enter state code: "))
+
+print(price)
